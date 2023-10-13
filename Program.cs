c2a2c6278abd47111f79b0210fa415c3b591ab75
mauri-rivera <mauri.rivera@outlook.com>
2023-10-13 11:11:15 -0300

    Update

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+List<object> listaValores = new List<object>();
+
+listaValores.Add(7);
+listaValores.Add(28);
+listaValores.Add(-1);
+listaValores.Add(true);
+listaValores.Add("silla");
+
+foreach (object val in listaValores) 
+{
+    Console.WriteLine(val);
+}
+
+Console.WriteLine("------------------------------------------------");
+
+int suma = 0;
+foreach (object num in listaValores)
+{
+    if (num is int)
+    {
+        suma += (int)num;
+    }
+}
+
+Console.WriteLine($"La suma es: {suma}");
+
+
